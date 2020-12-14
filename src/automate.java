b969c2c3f956e9bd81b474fbd734c6e44fad9459
sher95 <sher95c@gmail.com>
2020-12-14 14:45:38 +0500

    this is the automation for website

--- src/automate.java
+++ src/automate.java
@@ -1,16 +1,20 @@
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class automate {
 
     public static void main(String[] args) throws InterruptedException {
 
 
-        String picture = "C:\\Users\\Frank\\Desktop\\20200923_134105.jpg";
+        String picture0 = "picture loc";
+        String picture1 = "picture loc";
+        String picture2 = "picture loc";
         //To run chrome driver
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Frank\\Desktop\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "chrome loc");
         //For use opened chrome  in localhost
         ChromeOptions options = new ChromeOptions();
         options.setExperimentalOption("debuggerAddress", "localhost:9014");
@@ -30,13 +34,13 @@ public class automate {
 
         //Uploading pictures
         WebElement upload1 = driver.findElement(By.cssSelector("#app > div.v-application--wrap > main > div > div.container.d-block.white.container--fluid.fill-height.grid-list-md > div > div:nth-child(1) > div > div:nth-child(1) > div.col-sm-12.col-md-6.col-lg-6.col-xl-4.col > div > div > div:nth-child(4) > div > div.container > div > div.one > label.field__file-wrapper > div"));
-        upload1.sendKeys(picture);
+        upload1.sendKeys(picture0);
 
         WebElement upload2 = driver.findElement(By.xpath("//*[@id=\'app\']/div[1]/main/div/div[3]/div/div[1]/div/div[1]/div[2]/div/div/div[4]/div/div[3]/div/div[1]/label[2]/div"));
-        upload2.sendKeys("C:\\Users\\Frank\\Desktop\\20201209_214010.jpg");
+        upload2.sendKeys(picture1);
 
         WebElement upload3 = driver.findElement(By.xpath("//*[@id=\'app\']/div[1]/main/div/div[3]/div/div[1]/div/div[1]/div[2]/div/div/div[4]/div/div[3]/div/div[1]/label[2]/div"));
-        upload3.sendKeys("C:\\Users\\Frank\\Desktop\\20201209_213943.jpg");
+        upload3.sendKeys(picture2);
 
         Thread.sleep(2000);
         //submit
@@ -61,12 +65,12 @@ public class automate {
 
         //sms
         WebElement sms =driver.findElement(By.cssSelector("#app > div.v-application--wrap > main > div > div.container.d-block.white.container--fluid.fill-height.grid-list-md > div > div:nth-child(1) > div > div:nth-child(1) > div.col-sm-12.col-md-6.col-lg-6.col-xl-4.col > div > div > div:nth-child(8) > div > div.container > div > span:nth-child(5) > div > div > div > div"));
-        sms.sendKeys("998977318424");
+        sms.sendKeys("phone number");
         Thread.sleep(1000);
 
         //PIN code
         WebElement pin =driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/main/div/div[3]/div/div[1]/div/div[1]/div[2]/div/div/div[8]/div/div[2]/div/span[4]/div/div/div"));
-        pin.sendKeys("1111");
+        pin.sendKeys("pin code");
         Thread.sleep(8000);
         //click submit
         driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/main/div/div[3]/div/div[1]/div/div[1]/div[2]/div/div/div[8]/div/div[2]/div/button")).click();
